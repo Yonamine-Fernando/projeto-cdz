20999a776f12580766ff1f09d3c8b91fc534fd1c
Yonamine Fernando <yonamine.fernando@hotmail.com>
2024-06-12 05:44:02 +0900

    adicionado arquivos do projeto cdz

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+btnAvancar.addEventListener("click", function () {
+    const ehUltimoCartao = cartaoAtual === cartoes.length - 1;
+    if (ehUltimoCartao) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostrarCartao();
+});
+
+
+btnVoltar.addEventListener("click", function () {
+    let ehPrimeiroCartao = cartaoAtual === 0;
+    if (ehPrimeiroCartao) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual--;
+    mostrarCartao();
+
+});
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
